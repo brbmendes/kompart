a82ef68ec5eea4de97030eb81f33feb5a7e2a365
brbmendes <bbmendes@gmail.com>
2020-04-24 12:10:29 -0300

    Added server functions

--- organizacao_tp1.md
+++ organizacao_tp1.md
@@ -5,39 +5,44 @@
 - Um único programa, com 2 modos de operação (servidor e cliente)
 
 **Inicializar**
-- kompart <ip> <type>"s(server) || c(client)"
+- kompart <ip> <type>"s(server) || p(peer)"
 *OBS: Não foi realizado nenhum tipo de controle que identifique se já existe um server ativo ou não. Supõe-se que exista somente 1 server ativo.*
 
 **Comandos:**
-- registry client nick
-- registry path-to-file/filename
+
+* no servidor *
+- registry peer
+- registry file filename
 - list files
+
+* no peer *
 - get files[] | onde file é IP/filename ou IP/hash
 - get all IP
 
 **Funcionamento dos comandos**
-*registry client nick*
-	- Erros: nick ja cadastrado
-	Servidor busca a lista de peers pelo nick. Se não tiver nenhum peer com o nick solicitado, ele salva as informações do client no server. Se tiver, retorna a mensagem "Nick ja cadastrado"
+*registry peer*
+	- Erros: IP já registrado. Mensagem: "Peer <IP> já registrado"
+	Servidor busca a lista de peers pelo IP. Se não tiver nenhum peer com o IP solicitado, ele salva as informações do peer no server. Se tiver, retorna a mensagem [1].
 	
-*registry path-to-file/filename*
-	- Erros: client não registrado
-	- Funcionamento: O servidor deve buscar o client pelo IP. ao localizar, associa os arquivos enviados ao client. Se não localizar, retorna a mensagem "Client não registrado"
+*registry file filename*
+	- Erros: [1] Peer não registrado. Mensagem: "Peer <IP> não registrado"
+			[2] Arquivo já registrado Mensagem: "Arquivo nomeArquivo já registrado"
+	- Funcionamento: O servidor deve buscar o peer pelo IP. ao localizar, associa os arquivos enviados ao peer. Se não localizar peer, retorna a mensagem [1]. Se o arquivo já foi registrado, retorna a mensagem [2]. OBS: O peer digita "registry file filename", mas na verdade ele manda ao servidor "registry file filename hash"
 	
-	Ao realizar o comando, o client calcula o hash do arquivo e envia o nome completo (com caminho completo) e o hash do arquivo ao servidor.
+	Ao realizar o comando, o peer calcula o hash do arquivo e envia o nome completo (com caminho completo) e o hash do arquivo ao servidor.
 	
 *list files*
-	- Funcionamento: servidor informa a lista de arquivos (nick / nome / hash) em cada peer. Se não tiver arquivo registrado, exibe a mensagem "Sem arquivos registrados"
+	- Funcionamento: servidor informa a lista de arquivos (IP / nome / hash) em cada peer. Se não tiver arquivo registrado, exibe a mensagem "Sem arquivos registrados"
 
 *get files[]*
-	- Erros: [1] Client busca arquivo de outro client que já não está mais ativo. Retorna mensagem "Client (nick) offline".
-			[2] Client busca arquivo já removido. Retorna mensagem "arquivo removido ou indisponível".
-	- Funcionamento: Client A solicita um arquivo ao client B. Se o client B não estiver online, servidor retorna mensagem [1]. Se o arquivo não existir mais no client B, client B envia a mensagem [B]. Se existir, o client B envia os arquivos ao Client A.
+	- Erros: [1] Peer busca arquivo de outro peer que já não está mais ativo. Retorna mensagem "Peer <IP> offline".
+			[2] Peer busca arquivo já removido. Retorna mensagem "arquivo removido ou indisponível".
+	- Funcionamento: peer A solicita um arquivo ao peer B. Se o peer B não estiver online, servidor retorna mensagem [1]. Se o arquivo não existir mais no peer B, peer B envia a mensagem [B]. Se existir, o peer B envia os arquivos ao peer A.
 	
 *get all IP*
-	- Erros: 	[1] Client busca arquivo de outro client que já não está mais ativo. Retorna mensagem "Client (nick) offline".
-			[2] Client busca arquivo já removido. Retorna mensagem "arquivo removido ou indisponível".
-	- Funcionamento: Client A solicita um arquivo ao client B. Se o client B não estiver online, servidor retorna mensagem [1]. Se o arquivo não existir mais no client B, client B envia a mensagem [B]. Se existir, o client B envia todos os arquivos ao Client A.
+	- Erros: 	[1] Peer busca arquivo de outro peer que já não está mais ativo. Retorna mensagem "Peer <IP> offline".
+			[2] Peer busca arquivo já removido. Retorna mensagem "arquivo removido ou indisponível".
+	- Funcionamento: peer A solicita um arquivo ao peer B. Se o peer B não estiver online, servidor retorna mensagem [1]. Se o arquivo não existir mais no peer B, peer B envia a mensagem [B]. Se existir, o peer B envia todos os arquivos ao peer A.
 
 
 **Hearbeat**
@@ -47,23 +52,20 @@
 Utilizar uma thread separada que recebe as mensagens do servidor, e exibe na tela ao usuário.
 
 **Estruturas para armazenamento**
-*para servidor armazenar clientes*
-	hashmap<nick,peer>
+*para servidor armazenar peers*
+	hashmap<IP,peer>
 	
 	class peer
-		String nick;
         InetAddress addr;
 		int port;
 		int heartbeat;
 		bool active
-		hashmap<filename,file>
+		hashmap<filename,kfile>
 	
-	class file
+	class kfile
 		String filename;
-        string filedir;
 		string hash 
-		int size_in_bytes
 
-*para servidor armazenar a lista de recursos a serem enviados*
+*para peer armazenar a lista de recursos a serem enviados*
 arrayList<hashmap<peer destinatario,file arquivo>>
 
--- source/kompart.java
+++ source/kompart.java
@@ -5,177 +5,323 @@ import java.net.*;
 import java.util.*;
  
 public class kompart {
-
+	static Thread _threadHeartbeat;
+	static int _defaultHeartBeat;
     
 	public static void main(String[] args) throws IOException {
-		String recebido = null;
-		String mensagem = null;
+		if(args.length != 2){
+			System.out.println("Usage: java kompart <server IP> <type>");
+			System.out.println("Available types: s (run as server) and p (run as peer)");
+			return;
+		}
 
-		InetAddress endereco;
-		int porta;
-		byte[] texto = new byte[32768];
-		ArrayList<InetAddress> clientsToRemove = new ArrayList<>();
+		if(!args[1].toLowerCase().equals("s") && !args[1].toLowerCase().equals("p")){
+			System.out.println("Unrecognized type: " + args[1]);
+			System.out.println("\nUsage: java kompart <server IP> <type>");
+			System.out.println("Available types: s (run as server) and p (run as peer)");
+			return;
+		}
+
+		if(args[1].toLowerCase().equals("s"))
+			RunServer();
+		else
+			RunPeer(args[0]);
+	
+	}
+
+	public static void RunServer()  throws IOException {
+		String received = null;
+		String message = null;
+
+		InetAddress address;
+		int port;
+		byte[] text = new byte[32768];
+		ArrayList<InetAddress> peersToRemove = new ArrayList<>();
 		
 		DatagramSocket socket = new DatagramSocket(4500);
-		DatagramPacket pacote;
+		DatagramPacket packet;
 
-		HashMap<InetAddress,Client> clientes = new HashMap<>();
+		HashMap<InetAddress,Peer> peers = new HashMap<>();
 		int timerHeartbeat = 5;
 
 		while (true) {
 			try {
 				timerHeartbeat--;
-				// recebe datagrama
-				pacote = new DatagramPacket(texto, texto.length);
+				// receive datagram
+				packet = new DatagramPacket(text, text.length);
 				socket.setSoTimeout(500);
-				socket.receive(pacote);
+				socket.receive(packet);
 
-				// processa o que foi recebido
-				recebido = new String(pacote.getData(), 0, pacote.getLength());
-				endereco = pacote.getAddress();
-				porta = pacote.getPort();
+				// process received datagram
+				received = new String(packet.getData(), 0, packet.getLength());
+				address = packet.getAddress();
+				port = packet.getPort();
 
-				// armazena na variável dados o texto enviado
-				String[] dados = recebido.split(" ");
+				// store text as array
+				String[] splittedReceived = received.split(" ");
 
-				if(dados[0].equalsIgnoreCase("login")){
-					endereco = pacote.getAddress();
-					porta = pacote.getPort();
+				if(splittedReceived[0].equalsIgnoreCase("registry")){
+					if(splittedReceived[1].equals("peer")){
+						// registry peer
 
-					// adiciona o nick e o endereço do cliente no dicionario
-					clientes.put(endereco,new Client(dados[1],endereco,porta, 10));
+						// Check if peer has registered before
+						if(peers.containsKey(address)){
+							message = "Peer " + address + " already registered.";
+						} else {
+							// add IP to map of peers
+							peers.put(address,new Peer(address, port, _defaultHeartBeat));
 
-					// envia a resposta de volta ao cliente
-					mensagem = "Usuario logado. Digite 'list'.";
-					texto = mensagem.getBytes();
-					pacote = new DatagramPacket(texto, texto.length, endereco, porta);
-					socket.send(pacote);
-				} else if(dados[0].equalsIgnoreCase("list")) {
-					// percorre o dicionario e informa os clientes que estão logados
-					mensagem = "Usuarios logados: ";
-					for (Map.Entry<InetAddress,Client> pair : clientes.entrySet()) {
-						if(pair.getKey() != endereco){
-							mensagem += pair.getValue().nickname + ", ";
+							// send response to peer
+							message = "Peer registered.";
+						}
+
+						text = message.getBytes();
+						packet = new DatagramPacket(text, text.length, address, port);
+						socket.send(packet);
+					} else {
+						// registry file
+
+						// Check if peer has registered before
+						if(peers.containsKey(address)){
+							// Check if file has registered before
+							if(peers.get(address).GetFiles().containsKey(splittedReceived[2])){
+								message = "File " + splittedReceived[2] + " already registered.";
+							} else {
+								KFile kf = new KFile(splittedReceived[2], splittedReceived[3]);
+								peers.get(address).GetFiles().put(splittedReceived[2], kf);
+								message = "File " + splittedReceived[2] + " registered.";
+							}
+						} else {
+							message = "Peer " + address + " is not registered.";
+						}
+						text = message.getBytes();
+						packet = new DatagramPacket(text, text.length, address, port);
+						socket.send(packet);
+					}
+				} else if(splittedReceived[0].equalsIgnoreCase("list") && splittedReceived[1].equalsIgnoreCase("files")) {
+					// List all available files
+					message = "Available files: \n";
+					byte[] originalMessage = message.getBytes();
+					
+					for (Map.Entry<InetAddress,Peer> peer : peers.entrySet()) {
+						if(peer.getKey() != address){
+							message += peer.getValue().GetAddress() + "\n";
+							for(Map.Entry<String,KFile> file : peer.getValue().GetFiles().entrySet()){
+								message += "\t- filename: " + file.getValue().GetFileName() + " , hash: " + file.getValue().GetHash();
+							}
 						}
 					}
-					texto = mensagem.getBytes();
-					pacote = new DatagramPacket(texto, texto.length, endereco, porta);
-					socket.send(pacote);
-				} else if(dados[0].equalsIgnoreCase("exit")){
-					if(clientes.containsKey(endereco)){
-						clientes.remove(endereco);
+
+					if(Arrays.equals(originalMessage, message.getBytes())){
+						message = "No registered files.";
 					}
-					System.out.print("Usuarios logados: ");
-					for (Map.Entry<InetAddress,Client> pair : clientes.entrySet()) {
-						System.out.println(pair.getValue().GetNickname() + "hearbeat: " + pair.getValue().GetHeartbeat());
+					
+					text = message.getBytes();
+					packet = new DatagramPacket(text, text.length, packet.getAddress(), packet.getPort());
+					socket.send(packet);
+				} else if(splittedReceived[0].equalsIgnoreCase("exit")){
+					if(peers.containsKey(address)){
+						peers.remove(address);
 					}
-					// envia a resposta de volta ao cliente
-					mensagem = "Usuario deslogado com sucesso.";
-					texto = mensagem.getBytes();
-					pacote = new DatagramPacket(texto, texto.length, endereco, porta);
-					socket.send(pacote);
-				} else if(dados[0].equalsIgnoreCase("break")){
+					// send response to peer
+					message = "Peer removed.";
+					text = message.getBytes();
+					packet = new DatagramPacket(text, text.length, address, port);
+					socket.send(packet);
+				} else if(splittedReceived[0].equalsIgnoreCase("break")){
 					break;
-				} else if(dados[0].equalsIgnoreCase("heartbeat")){
-					endereco = pacote.getAddress();
-					clientes.get(endereco).SetHeartbeat(10);
-				} else if(dados[0].startsWith("/")){
-					String nick = dados[0].split("/")[1];
-					System.out.println(nick);
-					for (Map.Entry<InetAddress,Client> pair : clientes.entrySet()) {
-						if(pair.getValue().nickname.equalsIgnoreCase(nick)){
-							// pega end do destinatario		
-							InetAddress endDest = pair.getValue().GetEndereco();
-							// pega porta to destinatario
-							int portaDest = pair.getValue().GetPorta();
-							// pega end do remetente
-							endereco = pacote.getAddress();
-							// pega mensagem e concatena com o nick
-							String msg = new String(pacote.getData(), 0, pacote.getLength());
-							int indexOf = msg.indexOf(" ", 0);
-							msg = msg.substring(indexOf);
-							msg = "Msg de " + clientes.get(endereco).GetNickname() + ": " + msg;
-							// converte a mensagem
-							texto = msg.getBytes();
-							// envia pacote
-							pacote = new DatagramPacket(texto, texto.length, endDest, portaDest);
-							socket.send(pacote);
-							break;
-						}
-					}
+				} else if(splittedReceived[0].equalsIgnoreCase("heartbeat")){
+					address = packet.getAddress();
+					peers.get(address).SetHeartbeat(_defaultHeartBeat);
 				} else {
-					// envia a resposta de volta ao cliente
-					mensagem = "Comando não identificado. Tente 'login nick' ou list.";
-					texto = mensagem.getBytes();
-					pacote = new DatagramPacket(texto, texto.length, endereco, porta);
-					socket.send(pacote);
+					// send response to peer
+					message = "Unrecognized command.";
+					text = message.getBytes();
+					packet = new DatagramPacket(text, text.length, address, port);
+					socket.send(packet);
 				}
 			} catch (IOException e) {
 				if(timerHeartbeat <= 0){
 					timerHeartbeat = 5;
-					for (Map.Entry<InetAddress,Client> pair : clientes.entrySet()) {
-						// decrementa o hearbeat;
+					for (Map.Entry<InetAddress,Peer> pair : peers.entrySet()) {
+						// decrement hearbeat;
 						int hearbeat = pair.getValue().GetHeartbeat();
 						pair.getValue().SetHeartbeat(hearbeat-1);
-						System.out.println(pair.getValue().GetNickname() + " hearbeat: " + pair.getValue().GetHeartbeat());
-						// marca os clients para remover se heartbeat for 0
+						System.out.println(pair.getValue().GetAddress() + " hearbeat: " + pair.getValue().GetHeartbeat());
+						// mark peers to remove if heartbeat equals 0
 						if(pair.getValue().GetHeartbeat() == 0){
-							clientsToRemove.add(pair.getKey());
+							peersToRemove.add(pair.getKey());
 						}
 					}
 
-					// remove os clientes
-					for (InetAddress inetAddress : clientsToRemove) {
-						System.out.println("removendo: " + inetAddress);
-						clientes.remove(inetAddress);
+					// remove peers
+					for (InetAddress inetAddress : peersToRemove) {
+						System.out.println("removing: " + inetAddress);
+						peers.remove(inetAddress);
 					}
 
-					// limpa a lista de clientes para remover
-					clientsToRemove.clear();
+					// clean peer's list to remove
+					peersToRemove.clear();
 
-					System.out.println("Usuarios logados:");
-					for (Map.Entry<InetAddress,Client> pair : clientes.entrySet()) {
-						System.out.println(pair.getValue().GetNickname() + " hearbeat: " + pair.getValue().GetHeartbeat());
+					// print registered users
+					System.out.println("Registered users:");
+					for (Map.Entry<InetAddress,Peer> pair : peers.entrySet()) {
+						System.out.println(pair.getValue().GetAddress() + " hearbeat: " + pair.getValue().GetHeartbeat());
 					}
 				}
-				
 			}
 		}
 
 		socket.close();
 		System.out.println("Servidor finalizado...");
-    }
+	}
+	
+	public static void RunPeer(String serverIp)  throws IOException {
+		int a = 2;
+		System.out.println("entrou no client. ip: " + serverIp);
+		if(a == 2){
+			return;
+		}
+		
+		String input = "";
+		Scanner in = new Scanner(System.in);
+		byte[] texto = new byte[32768];
+		byte[] textoSend = new byte[32768];
+		// cria um socket datagrama
+		DatagramSocket socket = new DatagramSocket();
+
+		System.out.println("Bem vindo ao chat.\nPara logar digite 'login nickname'\n");
+		do{
+			input = in.nextLine();
+			if(!input.contains("login")){
+				System.out.println("Para logar digite 'login nickname'\n");
+			}
+		} while(!input.contains("login"));
+
+		// envia um pacote
+		textoSend = input.getBytes();
+		InetAddress endereco = InetAddress.getByName(serverIp);
+		DatagramPacket pacote = new DatagramPacket(textoSend, textoSend.length, endereco, 4500);
+		socket.send(pacote);
+
+		// Inicia thread do Heartbeat
+		_threadHeartbeat = StartHeartbeat(endereco);
+		_threadHeartbeat.start();
+
+		while (true) {
+			try {
+				if(input.equalsIgnoreCase("break")){
+					// fecha o socket
+					socket.close();
+					// fecha o scanner
+					in.close();
+					break;
+				}
+				
+				texto = new byte[32768];
+				// obtem a resposta
+				pacote = new DatagramPacket(texto, texto.length);
+				socket.setSoTimeout(500);
+				socket.receive(pacote);
+				
+				// mostra a resposta
+				String resposta = new String(pacote.getData(), 0, pacote.getLength());
+				System.out.println(resposta);
+				
+				if(input.equalsIgnoreCase("exit")){
+					// fecha o socket
+					socket.close();
+					// fecha o scanner
+					in.close();
+					break;
+				}
+
+				// o client fica preso na input, esperando digitar.
+				
+				input = in.nextLine();
+
+				textoSend = input.getBytes();
+				endereco = InetAddress.getByName(serverIp);
+				pacote = new DatagramPacket(textoSend, textoSend.length, endereco, 4500);
+				socket.send(pacote);
+			} catch (IOException e) {
+				
+			}
+		}
+		_threadHeartbeat.interrupt();
+		System.out.println("\nPrograma encerrado.\n");
+	}
+
+	public static Thread StartHeartbeat(InetAddress endereco) throws IOException {
+		return new Thread() {
+    
+			@Override
+			public void run() {
+				try{
+					String input = "";
+					int heartbeat = 5;
+
+					byte[] textoSend = new byte[32768];
+					DatagramSocket socket = new DatagramSocket();
+					DatagramPacket pacote;
+					
+					while(true){
+						heartbeat--;
+						if(heartbeat <= 0){				
+							heartbeat = 5;
+							input = "heartbeat";
+							textoSend = input.getBytes();
+							pacote = new DatagramPacket(textoSend, textoSend.length, endereco, 4500);
+							socket.send(pacote);
+						}
+						Thread.sleep(1000);
+					}
+				}
+				catch(SocketException se){
+					se.printStackTrace();
+				} catch(IOException ioe){
+					ioe.printStackTrace();
+				} catch (InterruptedException e) {
+					
+				}
+			}
+		};
+	}
     
     public static class Peer {
-        String nick;
         InetAddress addr;
 		int port;
 		int heartbeat;
 		Boolean isActive;
-		Hashmap<String,File>;
+		HashMap<String,KFile> files;
         
-        public Peer(String nickname, InetAddress address, int port, int heartbeat){
-            SetNickname(nickname);
+        public Peer(InetAddress address, int port, int heartbeat){
             SetAddress(address);
 			SetPort(port);
 			SetHeartbeat(heartbeat);
-        }
-        
-        public String GetNickname(){
-            return this.nick;
-        }
-
+			SetIsActive(true);
+			files = new HashMap<>();
+		}
+		
         public InetAddress GetAddress(){
             return this.addr;
         }
 
         public int GetPort(){
             return this.port;
-        }
+		}
+		
+		public int GetHeartbeat(){
+            return this.heartbeat;
+		}
 
-        public void SetNickname(String nickname){
-            this.nick = nickname;
-        }
+		public Boolean GetIsActive(){
+            return this.isActive;
+		}
+
+		public HashMap<String,KFile> GetFiles(){
+            return this.files;
+		}
 
         public void SetAddress(InetAddress address){
             this.addr = address;
@@ -185,58 +331,40 @@ public class kompart {
             this.port = port;
 		}
 		
-		public int GetHeartbeat(){
-            return this.heartbeat;
-		}
-		
 		public void SetHeartbeat(int heartbeat){
             this.heartbeat = heartbeat;
 		}
+
+		public void SetIsActive(Boolean isActive){
+            this.isActive = isActive;
+		}
+
+		
     }
 
-	public static class File{
+	public static class KFile{
 		String filename;
-        String path;
 		String hash;
-		byte[] size;
 
-		public File(String file, String path, String hash, byte[] size){
+		public KFile(String file, String hash){
 			SetFileName(file);
-			SetPath(path);
 			SetHash(hash);
-			SetSize(size);
 		}
 
 		public String GetFileName(){
 			return this.filename;
 		}
 
-		public String GetPath(){
-			return this.path;
-		}
-
 		public String GetHash(){
-			return this.path;
-		}
-
-		public byte[] GetSize(){
-			return this.size;
+			return this.hash;
 		}
 
 		public void SetFileName(String fileName){
 			this.filename = fileName;
 		}
 
-		public void SetPath(String path){
-			this.path = path;
-		}
-
 		public void SetHash(String hash){
 			this.hash = hash;
-		}
-
-		public void SetSize(byte[] size){
-			this.size = size;
 		}
 
 	}
